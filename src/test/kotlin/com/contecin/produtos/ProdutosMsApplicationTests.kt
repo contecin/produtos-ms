0a52a3895afa2c23832b0c4b9a3a49d01883b99a
diego_protec <diegosfprotec@gmail.com>
2023-03-23 19:11:33 -0300

    primeira versão do serviço com registro no eureka local

---
+++ src/test/kotlin/com/contecin/produtos/ProdutosMsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.contecin.produtos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProdutosMsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
